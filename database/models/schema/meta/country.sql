44ca5a9a62d31cde6ced5fcc76fba16d334463df
E33605 <debmalyapramanik.005@gmail.com>
2025-02-19 15:54:54 +0530

    📃 add init sql note for country table

--- database/models/schema/meta/country.sql
+++ database/models/schema/meta/country.sql
@@ -1,3 +1,24 @@
+/********************************************************************
+Global World Country States Cities Table Details
+
+The following table contains the list of continents, regions, country,
+states and cities details which can be used for various functionality
+and may also serve as indexation for transactional data.
+
+CHANGELOG: All major changes related to this file is documented here,
+(recommended to be used in a standalone query file) and follows the
+sementic versioning (https://semver.org/).
+    * v1.0.0: The initial production release. The following features
+      and details are available:
+        - 
+
+Author  : Debmalya Pramanik
+Contact : email@example.com
+Version : v1.0.0
+
+Copywright © [2025] Debmalya Pramanik, DigitPhilia INC
+********************************************************************/
+
 CREATE TABLE [meta].[mw_country] (
     [country_code] CHAR(3),
     [country_name] VARCHAR(64) NOT NULL,
