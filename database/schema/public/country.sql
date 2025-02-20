ebdd70ac723815b5b72a522965ba98e5f3497bcd
E33605 <debmalyapramanik.005@gmail.com>
2025-02-20 16:00:49 +0530

    🎉 add continent, regions and subregions table schema

--- database/schema/public/country.sql
+++ database/schema/public/country.sql
@@ -19,6 +19,72 @@ Version : v1.0.0
 Copywright © [2025] Debmalya Pramanik, DigitPhilia INC
 ********************************************************************/
 
+CREATE TABLE IF NOT EXISTS public.mw_continent (
+    continent_code
+        CHAR(2)
+        CONSTRAINT pk_continent_code PRIMARY KEY,
+
+    continent_name
+        VARCHAR(64)
+        CONSTRAINT uq_continent_name UNIQUE,
+    
+    wikidata_id
+        VARCHAR(16)
+        CONSTRAINT uq_continent_wikidata_id UNIQUE
+);
+
+
+CREATE TABLE IF NOT EXISTS public.mw_region (
+    region_code
+        CHAR(3)
+        CONSTRAINT pk_region_code PRIMARY KEY,
+
+    region_name
+        VARCHAR(64)
+        CONSTRAINT uq_region_name UNIQUE,
+
+    continent_code
+        CHAR(2)
+        CONSTRAINT fk_region_continent_code
+            REFERENCES public.mw_continent(continent_code)
+            ON UPDATE CASCADE
+            ON DELETE SET NULL,
+    
+    wikidata_id
+        VARCHAR(16)
+        CONSTRAINT uq_region_wikidata_id UNIQUE
+);
+
+
+CREATE TABLE IF NOT EXISTS public.mw_subregion (
+    subregion_code
+        CHAR(3)
+        CONSTRAINT pk_subregion_code PRIMARY KEY,
+
+    subregion_name
+        VARCHAR(64)
+        CONSTRAINT uq_subregion_name UNIQUE,
+
+    region_code
+        CHAR(3)
+        CONSTRAINT fk_subregion_region_code
+            REFERENCES public.mw_region(region_code)
+            ON UPDATE CASCADE
+            ON DELETE SET NULL,
+
+    continent_code
+        CHAR(2)
+        CONSTRAINT fk_subregion_continent_code
+            REFERENCES public.mw_continent(continent_code)
+            ON UPDATE CASCADE
+            ON DELETE SET NULL,
+    
+    wikidata_id
+        VARCHAR(16)
+        CONSTRAINT uq_subregion_wikidata_id UNIQUE
+);
+
+
 CREATE TABLE IF NOT EXISTS public.mw_country (
     country_code
         CHAR(3)
@@ -27,6 +93,27 @@ CREATE TABLE IF NOT EXISTS public.mw_country (
     country_name
         VARCHAR(64)
         CONSTRAINT uq_country_name UNIQUE,
+
+    subregion_code
+        CHAR(3)
+        CONSTRAINT fk_country_subregion_code
+            REFERENCES public.mw_subregion(subregion_code)
+            ON UPDATE CASCADE
+            ON DELETE SET NULL,
+    
+    region_code
+        CHAR(3)
+        CONSTRAINT fk_country_region_code
+            REFERENCES public.mw_region(region_code)
+            ON UPDATE CASCADE
+            ON DELETE SET NULL,
+
+    continent_code
+        CHAR(2)
+        CONSTRAINT fk_country_continent_code
+            REFERENCES public.mw_continent(continent_code)
+            ON UPDATE CASCADE
+            ON DELETE SET NULL,
 
     iso2_code
         CHAR(2)
