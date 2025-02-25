d3c41d23160c85c8c2256dc4c8b1b987145bbee3
E33605 <debmalyapramanik.005@gmail.com>
2025-02-25 14:59:30 +0530

    🛠⚙💣 explicitly state not null constraint, change column data type

--- database/schema/public/country.sql
+++ database/schema/public/country.sql
@@ -7,7 +7,7 @@ and may also serve as indexation for transactional data.
 
 Author  : Debmalya Pramanik
 Contact : email@example.com
-Version : v1.0.0
+Version : v1.0.1
 
 Copywright © [2025] Debmalya Pramanik, DigitPhilia INC
 ********************************************************************/
@@ -18,11 +18,11 @@ CREATE TABLE IF NOT EXISTS public.mw_continent (
         CONSTRAINT pk_continent_code PRIMARY KEY,
 
     continent_name
-        VARCHAR(64)
+        VARCHAR(64) NOT NULL
         CONSTRAINT uq_continent_name UNIQUE,
-    
+
     wikidata_id
-        VARCHAR(16)
+        VARCHAR(8) NOT NULL
         CONSTRAINT uq_continent_wikidata_id UNIQUE
 );
 
@@ -33,18 +33,18 @@ CREATE TABLE IF NOT EXISTS public.mw_region (
         CONSTRAINT pk_region_code PRIMARY KEY,
 
     region_name
-        VARCHAR(64)
+        VARCHAR(64) NOT NULL
         CONSTRAINT uq_region_name UNIQUE,
 
     continent_code
-        CHAR(2)
+        CHAR(2) NOT NULL
         CONSTRAINT fk_region_continent_code
             REFERENCES public.mw_continent(continent_code)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
-    
+
     wikidata_id
-        VARCHAR(16)
+        VARCHAR(8) NOT NULL
         CONSTRAINT uq_region_wikidata_id UNIQUE
 );
 
@@ -55,25 +55,25 @@ CREATE TABLE IF NOT EXISTS public.mw_subregion (
         CONSTRAINT pk_subregion_code PRIMARY KEY,
 
     subregion_name
-        VARCHAR(64)
+        VARCHAR(64) NOT NULL
         CONSTRAINT uq_subregion_name UNIQUE,
 
     continent_code
-        CHAR(2)
+        CHAR(2) NOT NULL
         CONSTRAINT fk_subregion_continent_code
             REFERENCES public.mw_continent(continent_code)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
 
     region_code
-        CHAR(3)
+        CHAR(3) NOT NULL
         CONSTRAINT fk_subregion_region_code
             REFERENCES public.mw_region(region_code)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
-    
+
     wikidata_id
-        VARCHAR(16)
+        VARCHAR(8) NOT NULL
         CONSTRAINT uq_subregion_wikidata_id UNIQUE
 );
 
@@ -84,38 +84,38 @@ CREATE TABLE IF NOT EXISTS public.mw_country (
         CONSTRAINT pk_country_code PRIMARY KEY,
 
     country_name
-        VARCHAR(64)
+        VARCHAR(64) NOT NULL
         CONSTRAINT uq_country_name UNIQUE,
 
     continent_code
-        CHAR(2)
+        CHAR(2) NOT NULL
         CONSTRAINT fk_country_continent_code
             REFERENCES public.mw_continent(continent_code)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
-    
+
     region_code
-        CHAR(3)
+        CHAR(3) NOT NULL
         CONSTRAINT fk_country_region_code
             REFERENCES public.mw_region(region_code)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
 
     subregion_code
-        CHAR(3)
+        CHAR(3) NOT NULL
         CONSTRAINT fk_country_subregion_code
             REFERENCES public.mw_subregion(subregion_code)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
 
     iso2_code
-        CHAR(2)
+        CHAR(2) NOT NULL
         CONSTRAINT uq_country_iso2 UNIQUE,
-    
+
     numeric_code
-        CHAR(3)
+        CHAR(3) NOT NULL
         CONSTRAINT uq_country_numeric_code UNIQUE,
-    
+
     top_level_domain
         CHAR(3)
         CONSTRAINT uq_country_top_level_domain UNIQUE
@@ -128,10 +128,10 @@ CREATE TABLE IF NOT EXISTS public.mw_state (
         CONSTRAINT pk_state_code PRIMARY KEY,
 
     state_name
-        VARCHAR(64),
+        VARCHAR(64) NOT NULL,
 
     country_code
-        CHAR(3)
+        CHAR(3) NOT NULL
         CONSTRAINT fk_state_country_code
             REFERENCES public.mw_country(country_code)
             ON UPDATE CASCADE
@@ -156,17 +156,17 @@ CREATE TABLE IF NOT EXISTS public.mw_city (
         CONSTRAINT pk_city_code PRIMARY KEY,
 
     city_name
-        VARCHAR(64),
+        VARCHAR(64) NOT NULL,
 
     country_code
-        CHAR(3)
+        CHAR(3) NOT NULL
         CONSTRAINT fk_city_country_code
             REFERENCES public.mw_country(country_code)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
 
     state_code
-        CHAR(5)
+        CHAR(5) NOT NULL
         CONSTRAINT fk_city_state_code
             REFERENCES public.mw_state(state_code)
             ON UPDATE CASCADE
