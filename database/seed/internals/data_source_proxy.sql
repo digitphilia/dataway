04299c4ae500b5cccee324e13ef7d5e984fff361
E33605 <debmalyapramanik.005@gmail.com>
2025-04-03 16:31:31 +0530

    ✨ add seed data for data source proxy table - check private repository for actual source details

---
+++ database/seed/internals/data_source_proxy.sql
@@ -0,0 +1,5 @@
+INSERT INTO internals.mw_data_source_proxy (data_source_proxy_id, data_source_proxy_desc) VALUES
+    (1, 'Default FOREX Rates Web Service'),
+    (2, 'Alternate (I) FOREX Rates Web Service'),
+    (3, 'Alternate (II) FOREX Rates Web Service'),
+    (4, 'Internal Subscribed Trade Data Source');
