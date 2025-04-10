d2fac6f62a215b37c4882ab426e624906c6d37a5
E33605 <debmalyapramanik.005@gmail.com>
2025-04-10 11:17:01 +0530

    ✨💣 add new material types, order by name for search

--- seed/projects/materials.sql
+++ seed/projects/materials.sql
@@ -1,7 +1,9 @@
 INSERT INTO projects.mw_material_type (material_type_id, material_type_name, material_type_desc) VALUES
-    ('RM', 'Raw Materials', 'Minimally processed to make finished goods.'),
     ('CM', 'Commodity Materials', 'Typically bought/sold in their natural form.'),
-    ('PM', 'Processed Materials', 'Typically bought/sold in their finished form.');
+    ('FG', 'Finished Goods', 'Typically bought/sold in their finished form.'),
+    ('PM', 'Processed Materials', 'Typically bought/sold in their finished form.'),
+    ('MC', 'Machinery Components', 'Typically bought/sold in their finished form.'),
+    ('RM', 'Raw Materials', 'Minimally processed to make finished goods.');
 
 
 INSERT INTO projects.mw_material_subtype (material_subtype_id, material_type_id, material_subtype_name, material_subtype_desc) VALUES
