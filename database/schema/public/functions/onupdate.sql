99b18d6c2433de5b12ab82ada610cc6d763d769e
E33605 <debmalyapramanik.005@gmail.com>
2025-04-16 17:46:44 +0530

    📝 update function documentation, #44

--- database/schema/public/functions/onupdate.sql
+++ database/schema/public/functions/onupdate.sql
@@ -13,9 +13,22 @@ However, the same can be implemented by using a function and a trigger
 which is equivalent to the internal functionality of other database
 flavors like MySQL.
 
+The function is defined under the "public" schema as this trigger may
+be accessed by all the tables which may belong to a different schema.
 Ensure the field is always named as `updated_on` in the all the tables,
 else a seperate function need to be created. The trigger function is
 table specific and should be created for each table.
+
+.. code-block:: sql
+
+    CREATE TRIGGER <trigger-name> BEFORE UPDATE
+    ON <table-name> FOR EACH ROW EXECUTE PROCEDURE onupdate();
+
+Note, the function is written without a schema name, thus denoting
+that the function must be residing in the default postgres schema.
+However, in other database flavors like MS-SQL, the default schema may
+not be the "public" schema, in which case a refactoring is required.
+(More at [GH#44](https://github.com/digitphilia/dataway/issues/44)).
 
 Copywright © [2025] Debmalya Pramanik, DigitPhilia INC.
 ********************************************************************/
